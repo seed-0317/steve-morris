45247cffecd68c70868f9391e744d57fad4f6d6e
Morris <qzh225@784f4385bef8.cof.ds.capitalone.com>
2017-04-21 08:36:53 -0400

    Completed JS homework

--- homework/homework.js
+++ homework/homework.js
@@ -9,7 +9,16 @@ module.exports = homework;
   f(10) = 55
 */
 homework.fibonacci = function(n){
-
+     
+     if (n === 0) {
+        return 0;
+     }
+     else if (n===1) {
+         return 1;
+     } 
+     else {
+         return (homework.fibonacci(n-1) + homework.fibonacci(n-2));
+     }  
 };
 
 /*
@@ -20,7 +29,25 @@ homework.fibonacci = function(n){
   Don't use the Array sort() method... that would be lame.
 */
 homework.sort = function(array) {
-
+    
+    
+    var i = 0;
+    var j = 0;
+    var temp;
+    if (array.length !== undefined) {
+       var len = array.length;
+       var a2 = array;
+       for (i=0; i<len-1; i++) {      
+         for (j=i+1; j<len; j++) {
+             if (a2[j] < a2[i]) {
+                 temp = a2[i];
+                 a2[i] = a2[j];
+                 a2[j] = temp;
+             }     
+         }   
+       }
+    }
+    return a2;
 };
 
 /*
@@ -31,6 +58,11 @@ homework.sort = function(array) {
   f(3) = 6
 */
 homework.factorial = function(n){
+    var answer = 1;
+    for (var i=1; i<=n; i++) {
+        answer = answer * i;
+    }
+    return answer;
 
 };
 
@@ -46,6 +78,18 @@ homework.factorial = function(n){
 */
 homework.rotateLeft = function(array, n) {
 
+   var len = array.length;
+   var temp;
+   var i;
+   var j;
+   for (i=1; i<= n%len; i++){
+        temp = array[0]
+        for (j=0; j< len-1; j++) {
+             array[j] = array[j+1];    
+        }
+        array[len -1] = temp;
+   }
+   return array;
 };
 
 /*
@@ -69,5 +113,49 @@ homework.rotateLeft = function(array, n) {
   Return false if not balanced
 */
 homework.balancedBrackets = function(bracketsString){
-
+    var len = bracketsString.length;
+    var stack = [ ];
+    var stacklen= 0;
+    var pos = 0;
+    while (pos<len) {
+        var c = bracketsString[pos]
+        if (c === "[" || c === "{"  || c === "(" ) { //push onto stack 
+           stack[stacklen] = c;
+           stacklen ++;
+        }
+        else if (c === "]" && stacklen>0) {
+                if (stack[stacklen-1] === "[")  {// pop from stack
+                    stack[stacklen-1] = "";
+                    stacklen = stacklen-1;
+                }
+                else {
+                    return false;
+                }    
+        } 
+        else if (c === "}" && stacklen>0) {
+                if (stack[stacklen-1] === "{")  {// pop from stack
+                    stack[stacklen-1] = "";
+                    stacklen = stacklen-1;
+                }
+                else {
+                    return false;
+                }   
+        }
+        else if (c === ")" && stacklen>0) {
+                if (stack[stacklen-1] === "(")  {// pop from stack
+                    stack[stacklen-1] = "";
+                    stacklen = stacklen-1;
+                }
+                else {
+                    return false;
+                }  
+        }
+        else {
+             return  false;
+        }  
+        pos++; 
+    
+    }
+    
+    return (stacklen === 0 );
 };
